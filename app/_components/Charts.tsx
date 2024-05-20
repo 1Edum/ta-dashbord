3e75870c84a4ea0061508f097f997baf0b4eb86d
1Edum <eduardomatosdesouza2@gmail.com>
2024-05-20 15:24:48 -0300

    shead: create Charts component

--- app/_components/Charts.tsx
+++ app/_components/Charts.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import React from "react";
 import ReactApexChart from "react-apexcharts";
@@ -72,3 +72,76 @@ export const TemperatureChart: React.FC = () => {
     </div>
   );
 };
+
+export const UmidadeChart: React.FC = () => {
+  const series = [
+    {
+      name: "Umidade Média (SP)",
+      data: [74.7, 74.2, 76, 72.2, 65.4, 58.8, 51, 43.5, 46.5, 58.8, 74.5, 76],
+    },
+    {
+      name: "Umidade Registrada",
+      data: [19, 21, 20, 22, 25, 24, 23, 27, 29, 28, 12, 23,],
+    },
+  ];
+
+  const options = {
+    chart: {
+      type: "line",
+      height: 350,
+      toolbar: {
+        show: true,
+      },
+    },
+    title: {
+      text: "Variação da Umidade",
+      align: "left",
+      style: {
+        fontSize: "16px",
+        color: "black",
+      },
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Fev",
+        "Mar",
+        "Abr",
+        "Mai",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Set",
+        "Out",
+        "Nov",
+        "Dez"
+      ],
+      title: {
+        text: "Meses",
+        style: {
+          fontSize: "12px",
+        },
+      },
+    },
+    yaxis: {
+      title: {
+        text: "Umidade (%)",
+        style: {
+          fontSize: "12px",
+        },
+      },
+    },
+  };
+
+  return (
+    <div>
+      <ReactApexChart
+        options={options}
+        series={series}
+        type="line"
+        height={350}
+      />
+    </div>
+  );
+};
+
