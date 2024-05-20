0b791e4f03e129ae16b18e73cdf7a287d849884a
1Edum <eduardomatosdesouza2@gmail.com>
2024-05-20 15:30:20 -0300

    shead: add pages-details component in page temperatura and page umidade

--- app/temperatura/page.tsx
+++ app/temperatura/page.tsx
@@ -1,38 +1,17 @@
 import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-} from "../_components/ui/card";
+
 import { TemperatureChart } from "../_components/Charts";
+import PagesDetails from "../_components/pages-details";
+
+const title = "Temperatura"
+const description = "A temperatura é uma medida da energia térmica média das partículas em um corpo ou sistema. Ela reflete o grau de agitação das partículas: quanto maior a temperatura, maior a energia cinética daspartículas."
+const cardTitle = "Temperatura Média da Semana"
+const cardDescription ="24°"
+
 
 function Temperatura() {
   return (
-    <div className="space-y-2">
-      <div className="rounded-lg bg-green-400 px-1">
-        <h1 className="text-2xl md:text-3xl">Temperatura</h1>
-      </div>
-      <div className="flex flex-col gap-2 md:flex-row">
-        <Card>
-          <CardDescription className="text-xl px-2 text-black text-justify">
-            A temperatura é uma medida da energia térmica média das partículas
-            em um corpo ou sistema. Ela reflete o grau de agitação das
-            partículas: quanto maior a temperatura, maior a energia cinética das
-            partículas.
-          </CardDescription>
-        </Card>
-        <Card className="flex md:w-1/2 w-full p-1 flex-col items-center justify-between">
-          <CardHeader>
-            <CardTitle>Temperatura Média da Semana</CardTitle>
-          </CardHeader>
-          <CardDescription className="text-3xl ">24°</CardDescription>
-        </Card>
-      </div>
-        <div className="max-w-full">
-        <TemperatureChart />
-        </div>
-    </div>
+    <PagesDetails title={title} description={description} cardTitle={cardTitle} cardDescription={cardDescription} Component={TemperatureChart}/>
   );
 }
 
--- app/umidade/page.tsx
+++ app/umidade/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { UmidadeChart } from '../_components/Charts'
+import PagesDetails from '../_components/pages-details'
+
+
+
 
 function Umidade() {
+const title = "Umidade"
+const description = "A umidade é a quantidade de vapor d'água presente no ar. É uma variável crucial em diversas áreas como meteorologia, climatologia, agricultura, saúde e engenharia. "
+const cardTitle = "Umidade Média da Semana"
+const cardDescription = "43"
   return (
-    <div> Umidade</div>
+    <div> 
+          <PagesDetails title={title} description={description} cardTitle={cardTitle} cardDescription={cardDescription} Component={UmidadeChart}/>
+    </div>
   )
 }
 
