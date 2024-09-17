d8f5399f7e106082ba29f69c1c22ded5ea18cff3
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-17 19:28:42 -0300

    feat: updating page temperatura

--- app/temperatura/page.tsx
+++ app/temperatura/page.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+'use client'
 
-import { TemperatureChart } from "../_components/Charts";
+import React from "react";
 import PagesDetails from "../_components/pages-details";
+import ChartTemperatura from "../_components/chart-temperatura";
+import { Card, CardContent, CardHeader, CardTitle } from "../_components/ui/card";
 
 const title = "Temperatura"
 const description = "A temperatura é uma medida da energia térmica média das partículas em um corpo ou sistema. Ela reflete o grau de agitação das partículas: quanto maior a temperatura, maior a energia cinética daspartículas."
 const cardTitle = "Temperatura Média da Semana"
 const cardDescription ="24°"
 
-
 function Temperatura() {
   return (
-    <PagesDetails title={title} description={description} cardTitle={cardTitle} cardDescription={cardDescription} Component={TemperatureChart}/>
+    <section className="w-full space-y-10">
+      <PagesDetails title={title} description={description} cardTitle={cardTitle} cardDescription={cardDescription}/>
+      <Card className="flex flex-col items-center justify-center">
+        <CardHeader>
+          <CardTitle className="text-center">{cardTitle}</CardTitle>
+        </CardHeader>
+        <CardContent className="w-[90vh]">
+          <ChartTemperatura />
+        </CardContent>
+      </Card>
+    </section>
   );
 }
 
