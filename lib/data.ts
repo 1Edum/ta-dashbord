64d931f1a1f94c84dc11cca76e3c604d9329c2c2
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-17 18:55:59 -0300

    feat: create data.ts configs

---
+++ lib/data.ts
@@ -0,0 +1,102 @@
+export  const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#4ade80",
+  },
+}
+
+export const chartDataUmidade = [
+    { month: "January", desktop: 186 },
+    { month: "February", desktop: 305},
+    { month: "March", desktop: 237},
+    { month: "April", desktop: 73 },
+    { month: "May", desktop: 209},
+    { month: "June", desktop: 214 },
+  ]
+
+
+export const chartDataTemperatura = [
+  { month: "January", desktop: 35 },
+  { month: "February", desktop: 30},
+  { month: "March", desktop: 28},
+  { month: "April", desktop: 25 },
+  { month: "May", desktop: 28 },
+  { month: "June", desktop: 30 },
+]
+
+export const chartData = [
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "June", desktop: 30 },
+]
+
+export const invoices = [
+  {
+    person: "Daniel",
+    date: "12/04/2020",
+    classRoom: "2 Ano B"
+  },
+  {
+    person: "Ana",
+    date: "11/03/2020",
+    classRoom: "1 Ano A"
+  },
+  {
+    person: "Pedro",
+    date: "10/02/2020",
+    classRoom: "3 Ano C"
+  },
+  {
+    person: "Maria",
+    date: "09/01/2020",
+    classRoom: "2 Ano B"
+  },
+  {
+    person: "João",
+    date: "08/12/2019",
+    classRoom: "1 Ano B"
+  },
+  {
+    person: "Carla",
+    date: "07/11/2019",
+    classRoom: "3 Ano A"
+  },
+  {
+    person: "Lucas",
+    date: "06/10/2019",
+    classRoom: "2 Ano A"
+  },
+  {
+    person: "Beatriz",
+    date: "05/09/2019",
+    classRoom: "1 Ano C"
+  },
+  {
+    person: "Rafael",
+    date: "04/08/2019",
+    classRoom: "3 Ano B"
+  },
+  {
+    person: "Isabela",
+    date: "03/07/2019",
+    classRoom: "2 Ano C"
+  },
+  {
+    person: "Beatriz",
+    date: "05/09/2019",
+    classRoom: "1 Ano C"
+  },
+  {
+    person: "Rafael",
+    date: "04/08/2019",
+    classRoom: "3 Ano B"
+  },
+  {
+    person: "Isabela",
+    date: "03/07/2019",
+    classRoom: "2 Ano C"
+  }
+]
